14415d5ac12006e7e977432d07f524fb398408bb
Rajesh Borate <rajeshborate08@gmail.com>
2019-06-21 10:23:36 +0530

    Create tsunami.c

---
+++ tsunami.c
@@ -0,0 +1,19 @@
+/*In Japan, there was a very huge Tsunami. Millions and millions worth buildings and property were destroyed. Many people lost their lives while some of them were injured and few were safe. A news reporter arrives at the spot to take the current survey regarding the situation of the people alive, dead and injured. He wanted to publish it in the newspaper and reach out to other countries asking to help the affected people. Can you please help him in this noble cause by writing a program to generate the newspaper report ?
+
+
+
+INPUT & OUTPUT FORMAT:
+
+Input consists of three integers corresponding to the number of people dead, injured and those who are still alive and safe.
+Refer sample input and output for formatting specifications.
+*/
+
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    printf("1)Dead: %d\n2)Injured: %d\n3)Safe: %d\nPlease help the people who are suffering!!!",a,b,c);
+    //fill the code
+    return 0;
+}
