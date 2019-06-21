9d7a80e2f4aa4dc33edecb11a01c34bd8eae3592
Rajesh Borate <rajeshborate08@gmail.com>
2019-06-21 10:18:27 +0530

    Create integer.c

---
+++ integer.c
@@ -0,0 +1,10 @@
+//Write a program to get an integer from the user and display it.
+
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    printf("%d",a);
+    //file the code
+    return 0;
