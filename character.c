b0bea6f4b0b1ecfe77c2bb34260448fe67458fda
Rajesh Borate <rajeshborate08@gmail.com>
2019-06-21 10:17:35 +0530

    Create character.c

---
+++ character.c
@@ -0,0 +1,12 @@
+//Write a program to get a character value from the user and display it.
+
+
+#include<stdio.h>
+int main()
+{
+    char c;
+    scanf("%c",&c);
+    printf("%c",c);
+    //fill the code
+    return 0;
+}
