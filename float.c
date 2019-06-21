91240d38eaad1564f9fcba48109e1d5bd6273495
Rajesh Borate <rajeshborate08@gmail.com>
2019-06-21 10:16:15 +0530

    Create float.c

---
+++ float.c
@@ -0,0 +1,12 @@
+//Write a program to get a float value from the user and display it.
+
+
+#include<stdio.h>
+int main()
+{
+    float f;
+    scanf("%f",&f);
+    printf("%f",f);
+    //fill the code
+    return 0;
+}
