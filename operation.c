ce78b93c9f11b24f29bef9010f74b8d03bbefbb8
Rajesh Borate <rajeshborate08@gmail.com>
2019-06-21 10:26:12 +0530

    Create operation.c

---
+++ operation.c
@@ -0,0 +1,16 @@
+//Write a C program to perform arithmetic operations such as addition, subtraction, multiplication, modulo division and division.
+
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    printf("%d\n",a+b);
+    printf("%d\n",a-b);
+    printf("%d\n",a*b); 
+    printf("%d\n",a%b); 
+    printf("%d\n",a/b);
+    // fill the code
+    
+    return 0;
+}
