620b8134fa7f53aaf85eeaffcf87de469d2cfecf
Rajesh Borate <rajeshborate08@gmail.com>
2019-06-21 10:16:52 +0530

    Create decimal.c

---
+++ decimal.c
@@ -0,0 +1,15 @@
+//Write a program to get a float value from the user and display it in the below-mentioned format.
+
+#include<stdio.h>
+int main()
+{
+    float f;
+    scanf("%f",&f);
+    printf("%.6f\n",f);
+    printf("%.3f\n",f);
+    printf("%.2f\n",f);
+    printf("%.1f\n",f);
+    
+    //fill the code
+    return 0;
+}
