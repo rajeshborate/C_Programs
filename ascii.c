6182194468c87bccce298557cbd047d6cb92e8ed
Rajesh Borate <rajeshborate08@gmail.com>
2019-06-21 10:22:21 +0530

    Create ascii.c

---
+++ ascii.c
@@ -0,0 +1,11 @@
+//Write a C program to generate the equivalent ASCII value for a given character.
+
+#include<stdio.h>
+int main()
+{
+    char c;
+    scanf("%c",&c);
+    printf("%d",c);
+    //fill the code
+    return 0;
+}
