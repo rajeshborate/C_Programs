e9511614fc183165a7b88cafaedac1c055d0ff02
Rajesh Borate <rajeshborate08@gmail.com>
2019-06-29 18:14:54 +0530

    Create pa7.c

---
+++ pa7.c
@@ -0,0 +1,39 @@
+/*
+QUESTION:
+Write a C program to generate the following pattern.
+
+
+SAMPLE INPUT:
+
+Enter n value:
+5
+SAMPLE OUTPUT:
+
+1
+123
+12345
+1234567
+123456789
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j, N;
+    scanf("%d", &N);
+
+    for(i=1;i<=N;i++)
+    {
+        // Logic to print numbers
+        for(j=1;j<=(2*i)-1;j++)
+        {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
